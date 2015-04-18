c0cdc292c8086862a26b1aabb11765a376718950
ODROID <roman-dvorak@email.cz>
2015-04-19 00:59:42 +0200

    v1.9

--- multigen.py
+++ multigen.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
 	#gui.main()
 	os.chdir(r"./out/")
 	rmob = rmob.rmob()
-	rmob.setSftp('space.astro.cz', 'ZVPP')
+	rmob.setSftp('space.astro.cz', 'indexer')
 	for observatory in rmob.getObservatorys():
 		rmob.setGenPreferences(observatory)
 		rmob.setActual(True)
